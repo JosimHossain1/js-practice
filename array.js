970409722370db112f2bedf802df4c7ce72d12fc
Md Josim <josimhossain842@gmail.com>
2024-03-07 16:08:16 +0600

    Array Done

--- array.js
+++ array.js
@@ -1,4 +1,52 @@
+// // Declare an array
+// // const arr = [10, 20, 30, 40, 50]
 
-const arr = [10, 20, 30, 40, 50]
+// console.log(arr.length) // Check an array lenght
 
-console.log(arr.indexOf(10))
+
+// --------------------------------
+
+// Check a particular item index
+// const arr = [10, 20, 30, 40, 50]
+// console.log(arr.indexOf(30))
+
+
+// Add a new element in last of the array
+// const arr = [10, 20, 30, 40, 50]
+
+// arr.push("Josim")
+
+// console.log(arr)
+
+// Remove the last element from an array
+
+// const arr = [10, 20, 30, 40, 50]
+
+// arr.pop()
+// console.log(arr)
+
+// Add a element as a first item of array
+
+// const arr = [10, 20, 30, 40, 50]
+// arr.unshift("Josim")
+// console.log(arr)
+
+
+// Remove the first element from an array
+
+// const arr = [10, 20, 30, 40, 50]
+// arr.shift()
+// console.log(arr)
+
+// const arr = [10, 20, 30, 40, 50]
+
+// console.log(arr.includes(50))
+
+
+// Get the from by index
+// const arr = [10, 20, 30, 40, 50]
+// console.log(arr.at(3))
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// delete fruits[0];
+// console.log(fruits)
